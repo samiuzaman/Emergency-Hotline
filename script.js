e29ab5992223ad1a46800a0efc83fed895dbfcc4
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-13 21:25:10 +0600

    Added heart button functionality

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-console.log("Project Ready");
-const myFunction = () => {
-    console.log("Raht")
-}
-const rahat = document.getElementById("id").innerHTML = "Hello js";
+const cardContainer = document.getElementById("card-container");
+cardContainer.addEventListener("click", function (event) {
+  // heart count functionality
+  if (event.target.classList.contains("heart-btn")) {
+    const id = event.target.getAttribute("id");
+    const heartCount = document.getElementById("heart-count");
+    const totalHeart = parseInt(heartCount.innerText) + 1;
+    heartCount.textContent = totalHeart;
+    const heartButton = document.getElementById(id);
+    heartButton.classList.remove("text-gray-500", "fa-regular", "fa-heart");
+    heartButton.classList.add("text-red-500", "fa-solid", "fa-heart");
+    console.log(id);
+  }
+
+ 
+});
