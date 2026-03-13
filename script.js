3225b6b9cbadcf69277abd5dcf99a511801e0817
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-13 21:54:45 +0600

    modify heart button functionality

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const cardContainer = document.getElementById("card-container");
 cardContainer.addEventListener("click", function (event) {
   // heart count functionality
   if (event.target.classList.contains("heart-btn")) {
-    const id = event.target.getAttribute("id");
     const heartCount = document.getElementById("heart-count");
     const totalHeart = parseInt(heartCount.innerText) + 1;
     heartCount.textContent = totalHeart;
-    const heartButton = document.getElementById(id);
+    const heartButton = event.target;
     heartButton.classList.remove("text-gray-500", "fa-regular", "fa-heart");
     heartButton.classList.add("text-red-500", "fa-solid", "fa-heart");
-    console.log(id);
   }
 
   // Call Button Functionality
